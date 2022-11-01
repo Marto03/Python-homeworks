a4cc278204c55f171cb39e44e44c38ec6021b0dd
Marto03 <116292812+Marto03@users.noreply.github.com>
2022-11-01 22:13:40 +0200

    Update Second_exercise.py

--- Second_exercise.py
+++ Second_exercise.py
@@ -4,7 +4,7 @@ from math import pi
 r = float(input())
 perimeter = 2 * pi * r
 S = pi * r**2
-perimeter = str(round(perimeter, 3))
-S = str(round(S, 3))
+perimeter = float(round(perimeter, 3))
+S = float(round(S, 3))
 print(f'The S is :{S}')
 print(f'The perimeter is:{perimeter}')

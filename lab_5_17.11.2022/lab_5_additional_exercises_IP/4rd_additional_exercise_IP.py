196cf55bad032e31b7e11660a3e746c3f8a91a03
Marto03 <martaleksandrov@tu-sofia.com>
2022-11-21 21:08:24 +0200

    Homework from 17.11.2022

--- lab_5_17.11.2022/lab_5_additional_exercises_IP/4rd_additional_exercise_IP.py
+++ lab_5_17.11.2022/lab_5_additional_exercises_IP/4rd_additional_exercise_IP.py
@@ -1,18 +1,25 @@
-# задачата не работи , не успях да измисля как да я направя
-col_type = list[input("Input: ")]
+# задачата не работи , не успях да измисля как да я довърша
+
 def get_collection_builder(col_type):
 
     if isinstance(col_type, tuple):
         for text in range(1, 5):
-            current_parameter = input((f"Input parameter {text}: "))
+            current_parameter = input(f"Input parameter {text}: ")
+            tuple.__add__(current_parameter)
+        return tuple, col_type
 
-        return col_type
     elif isinstance(col_type, list):
+        list_1 = []
         for text in range(1, 5):
-            current_parameter = input([f"Input parameter {text}:"])
+            current_parameter = input(f"Input parameter {text}: ")
+            list_1.append(current_parameter)
+        return list_1, col_type
     elif isinstance(col_type, set):
+        set_1 = {}
         for text in range(1, 5):
-            current_parameter = input({f"Input parameter {text}: "})
+            current_parameter = input(f"Input parameter {text}: ")
+            set_1.add(current_parameter)
+        return set_1, col_type
     return col_type
 
-print(get_collection_builder(type(col_type)))
+print(get_collection_builder([list]))

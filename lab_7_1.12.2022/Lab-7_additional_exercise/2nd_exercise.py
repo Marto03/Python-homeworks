5cae35dcbcfe643d73f13a91b4bbcd15aa058984
Marto03 <martaleksandrov@tu-sofia.com>
2022-12-06 18:26:31 +0200

    Homework from 1.12.2022

--- lab_7_1.12.2022/Lab-7_additional_exercise/2nd_exercise.py
+++ lab_7_1.12.2022/Lab-7_additional_exercise/2nd_exercise.py
@@ -10,18 +10,38 @@ class InvalidFoodError(Exception):
 
 class Food():
     def __init__(self, carbs, protein, fats, salt):
-        self.carbs = carbs
-        self.protein = protein
-        self.fats = fats
-        self.salt = salt
-        if carbs != float or protein != float or fats != float or salt != float:
-            raise InvalidParameterError
+        self.carbs = float(carbs)
+        self.protein = float(protein)
+        self.fats = float(fats)
+        self.salt = float(salt)
+        if type(carbs) != float or type(protein) != float or type(fats) != float or type(salt) != float:
+           raise InvalidParameterError
+        if carbs > 100 or protein > 100 or fats > 100 or salt > 100:
+            raise NutrientError
+        if carbs == 0 and protein == 0 and fats == 0 and salt == 0:
+            raise InvalidFoodError
     def print_label(self):
         print(f"Food{self.carbs, self.protein, self.fats, self.salt}")
 #pizza = Food(3.2, 4.3, 6.1, 4.8)
 #pizza.print_label()
-try:
-    pizza = Food(3.2, 4.2, 6.1, 4.8)
-    pizza.print_label()
-except InvalidParameterError:
-    print("Wrong")
+
+#try:
+#    pizza = Food(3.2, 4.2, 6.1, 4.8)
+#    pizza.print_label()
+
+#except InvalidParameterError:
+#    print("Wrong")
+for i in range(0, 120, 10):
+    try:
+        pizza = Food(3.4, 78.3, 23.4, 56.1)
+
+        meat = Food(42.3, 20.8, 97.1, 2.3)
+    except InvalidParameterError:
+        print("Invalid parameter")
+    except NutrientError:
+        print("NutrientError")
+    except InvalidFoodError:
+        print("Invalid food")
+    else:
+        pizza.print_label()
+        meat.print_label()
